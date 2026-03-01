554de51c5b1ab3dc8b2755a4ecf7e37fd52670b6
Ritik Raj Srivastava <160333960+Ritikraj11@users.noreply.github.com>
2026-03-01 12:28:13 +0530

    Add files via upload

---
+++ 01_module_c/Readme.md
@@ -0,0 +1,7 @@
+Due to time management I have not done Javascript code
+
+--> Completed Home page 
+--> Completed Sector page
+--> Half Completed my plan page 
+
+Link to access---->
---
+++ 01_module_c/pages/sector/script.js
@@ -0,0 +1,28 @@
+let btn = document.getElementById('role-plan')
+
+data = [
+    {
+        name:"Information Techonology",
+        role:{
+            1:"Lab technician",
+            2:"computer Engineer",
+            3:"Cheif Expert"
+        }
+    },
+    {
+        name:"Construction",
+        role:{
+            1:"Civil Engineer",
+            2:"Cheif Engineer",
+            3:"Local WOrkers"
+        }
+    },
+    {
+        name:"Tourism and Hospitality",
+        role:{
+            1:"Lab technician",
+            2:"computer Engineer",
+            3:"Cheif Expert"
+        }
+    },
+]
